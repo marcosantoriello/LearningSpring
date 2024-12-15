dd11d03cdea07162c448c82588335567d8d6a3e3
marcosantoriello <santoriellomarco@yahoo.com>
2024-12-15 15:37:35 +0100

    Refactored data.sql so that it doesn't get executed

--- LibraryManagement/src/main/resources/TEMPdata.sql
+++ LibraryManagement/src/main/resources/TEMPdata.sql
@@ -1,2 +1,3 @@
+-- just for test purposes
 insert into Book (id, title, author, isbn, quantity) values (1, 'Spring in Action', 'Craig Walls', '9781617294945', 10);
 insert into Book (id, title, author, isbn, quantity) values (2, '1984', 'George Orwell', '9788804668237', 3);
